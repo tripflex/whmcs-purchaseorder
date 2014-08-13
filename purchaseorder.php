32b16b847094661ac3c67289963e315d57aef490
Myles McNamara <tripflex@users.noreply.github.com>
2014-08-13 14:49:55 -0400

    add sendregistrar true to api call

--- purchaseorder.php
+++ purchaseorder.php
@@ -11,25 +11,25 @@
 function purchaseorder_config() {
 
 	$configarray = array(
-		"FriendlyName" => array(
+		"FriendlyName"            => array(
 			"Type"  => "PO",
 			"Value" => "Purchase Order"
 		),
-		"approvedforpo" => array(
+		"approvedforpo"           => array(
 			"FriendlyName" => "Approved for PO Message",
 			"Type"         => "textarea",
 			"Rows"         => "5",
 			"Value"        => "<strong style=\"color: #008000;\">Verified Account</strong><br />Invoice will be marked as paid when payment is verified for the PO.",
 			"Description"  => "",
 		),
-		"notapprovedforpo" => array(
+		"notapprovedforpo"        => array(
 			"FriendlyName" => "Not Approved for PO Message",
 			"Type"         => "textarea",
 			"Rows"         => "5",
 			"Value"        => "<strong style=\"color: #BA3832;\">Invalid Account</strong><br />You are not authorized to pay via Purchase Order, please contact support, or choose another payment method.",
 			"Description"  => "",
 		),
-		"verifiedaccountfield" => array(
+		"verifiedaccountfield"    => array(
 			"FriendlyName" => "Approved for PO Client Custom Field",
 			"Type"         => "dropdown",
 			"Description"  => "Select the custom field used to Verify/Approve a client to use Purchase Orders.",
@@ -39,33 +39,35 @@ function purchaseorder_config() {
 			"FriendlyName" => "Enable Auto Activate",
 			"Type"         => "yesno"
 		),
-	    "autoactivatefield" => array(
-		    "FriendlyName" => "Auto Activate Client Custom Field",
-	        "Type" => "dropdown",
-	        "Description" => "Select the custom client field used for auto activation",
-	        "Options" => purchaseorder_get_custom_fields()
-	    )
+		"autoactivatefield"       => array(
+			"FriendlyName" => "Auto Activate Client Custom Field",
+			"Type"         => "dropdown",
+			"Description"  => "Select the custom client field used for auto activation",
+			"Options"      => purchaseorder_get_custom_fields()
+		)
 	);
 
 	return $configarray;
 
 }
 
-function purchaseorder_get_custom_fields(){
-	$options = '';
-	$request  = full_query( "SELECT GROUP_CONCAT(fieldname) FROM tblcustomfields where type='client' GROUP BY type" );
-	$values = mysql_fetch_array( $request );
+function purchaseorder_get_custom_fields() {
 
-	return $values[0];
+	$options = '';
+	$request = full_query( "SELECT GROUP_CONCAT(fieldname) FROM tblcustomfields where type='client' GROUP BY type" );
+	$values  = mysql_fetch_array( $request );
+
+	return $values[ 0 ];
 
 }
 
 function purchaseorder_accept_order( $orderid ) {
 
-	$command               = "acceptorder";
-	$values[ "orderid" ]   = $orderid;
-	$values[ "autosetup" ] = TRUE;
-	$values[ "sendemail" ] = TRUE;
+	$command                   = "acceptorder";
+	$values[ "orderid" ]       = $orderid;
+	$values[ "autosetup" ]     = TRUE;
+	$values[ "sendregistrar" ] = TRUE;
+	$values[ "sendemail" ]     = TRUE;
 
 	$results = localAPI( $command, $values );
 
@@ -87,19 +89,19 @@ function purchaseorder_orderformcheckout( $params ) {
 function purchaseorder_link( $params ) {
 
 	global $CONFIG;
-	$code      = "";
-	$cart_action = filter_input( INPUT_GET, 'a', FILTER_SANITIZE_STRING );
-	$actual_auto_activate_value = array();
+	$code                          = "";
+	$cart_action                   = filter_input( INPUT_GET, 'a', FILTER_SANITIZE_STRING );
+	$actual_auto_activate_value    = array();
 	$actual_verified_account_value = array();
 
 	$sysurl    = ( $CONFIG[ 'SystemSSLURL' ] ? $CONFIG[ 'SystemSSLURL' ] : $CONFIG[ 'SystemURL' ] );
 	$invoiceid = $params[ 'invoiceid' ];
 
-	if( $params['enableautoactivatefield'] == 'on' && ! empty( $params[ 'autoactivatefield' ] ) ){
+	if ( $params[ 'enableautoactivatefield' ] == 'on' && ! empty( $params[ 'autoactivatefield' ] ) ) {
 
-		$auto_activate_field_label = $params['autoactivatefield'];
-		$auto_activate_query = select_query( 'tblcustomfields', 'id', array( 'fieldname' => $auto_activate_field_label ) );
-		$auto_activate_id = mysql_fetch_array( $auto_activate_query );
+		$auto_activate_field_label = $params[ 'autoactivatefield' ];
+		$auto_activate_query       = select_query( 'tblcustomfields', 'id', array( 'fieldname' => $auto_activate_field_label ) );
+		$auto_activate_id          = mysql_fetch_array( $auto_activate_query );
 
 	}
 
@@ -115,12 +117,12 @@ function purchaseorder_link( $params ) {
 //	echo "VERIFIED ACCOUNT ID =" . $verified_account_id;
 
 	foreach ( $params[ 'clientdetails' ][ 'customfields' ] as $customfield ) {
-		if ( $customfield[ 'id' ] == $auto_activate_id[0] ) {
+		if ( $customfield[ 'id' ] == $auto_activate_id[ 0 ] ) {
 			$actual_auto_activate_value = $customfield[ 'value' ];
 		}
 
-		if( $customfield[ 'id' ] == $verified_account_id[0] ) {
-			$actual_verified_account_value = $customfield['value'];
+		if ( $customfield[ 'id' ] == $verified_account_id[ 0 ] ) {
+			$actual_verified_account_value = $customfield[ 'value' ];
 		}
 	}
 
@@ -132,15 +134,15 @@ function purchaseorder_link( $params ) {
 	// Make sure you set the custom field value above
 	if ( $actual_verified_account_value == 'on' ) {
 
-		$code .= html_entity_decode($params['approvedforpo']);
+		$code .= html_entity_decode( $params[ 'approvedforpo' ] );
 
 	} else {
 
-		$code .= html_entity_decode($params['notapprovedforpo']);
+		$code .= html_entity_decode( $params[ 'notapprovedforpo' ] );
 
 	}
 
-	if( $actual_auto_activate_value == 'on' && ! empty( $orderid ) && $cart_action != 'complete' ){
+	if ( $actual_auto_activate_value == 'on' && ! empty( $orderid ) && $cart_action != 'complete' ) {
 		purchaseorder_accept_order( $orderid );
 		logModuleCall( 'autoactivate', 'activate', 'accepting order...', $orderid );
 	}
